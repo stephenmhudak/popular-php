5c2dad8b82f8e6e1b1c60ff26d784c644b5153c9
stephenmhudak <60153794+stephenmhudak@users.noreply.github.com>
2023-01-21 15:33:04 -0600

    Begin boilerplate

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 ## Steps of creating project
 1. Start new laravel project
 2. Create base for GitHub API calls
-3. Create model, migration, and controller
+3. Create index.blade.php
+4. Create model, migration, and controller
+5. Install tailwindcss and postcss
+6. Set default route to index.blade.php
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>1000 Most Popular PHP Repositories on GitHub</title>
+    @vite(['resources/css/app.css'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+</head>
+<body>
+    <div class="container">
+        <form action="">
+            <button>Update Repo List</button>
+        </form>
+    </div>
+</body>
+</html>
